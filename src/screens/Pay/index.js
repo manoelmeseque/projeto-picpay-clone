c38c64d0e2931ebfa16f36bfafba2b3e9cee71c2
mesequeteixeira <57111262+manoelmeseque@users.noreply.github.com>
2021-07-28 12:19:22 -0300

    first commit, barra de navegação

---
+++ src/screens/Pay/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+export default function Pay(){
+    return <Text style={{justifyContent: 'center', alignItems: 'center', backgroundColor: '#ffac00'}}>Pay</Text>
+}
