71b0da9f78697c647562a125f0b208f9bd4e54cf
mesequeteixeira <57111262+manoelmeseque@users.noreply.github.com>
2021-09-06 12:25:38 -0300

    add banner Wallet

--- src/screens/Wallet/index.js
+++ src/screens/Wallet/index.js
@@ -1,6 +1,26 @@
 import React from 'react'
 import { Text } from 'react-native'
+import { Container, Banner, Section, BannerLabel, BannerCDI, BannerBalance, BannerCashBack, FormsPayment } from './styles'
 
 export default function Wallet(){
-    return <Text>Wallet</Text>
+    return (
+        <Container>
+
+            <Banner>
+
+                <BannerLabel>Instituição 000 . PicPay Seriviços S.A</BannerLabel>
+                <BannerLabel>Ag 0000 . Cc 000000000</BannerLabel>
+                <BannerCDI>Saldo rendendo 120% do CDI</BannerCDI>
+                <BannerBalance>R$0,00</BannerBalance>
+                <BannerCashBack>R$0,00 desse saldo é de CashBack ></BannerCashBack>
+
+            </Banner>
+            <Section>
+
+                <FormsPayment>Formas de pagamento</FormsPayment>
+
+            </Section>
+
+        </Container>
+    )
 }
---
+++ src/screens/Wallet/styles.js
@@ -0,0 +1,51 @@
+import styled from 'styled-components/native'
+import { LinearGradient } from 'expo-linear-gradient'
+
+export const Container = styled.SafeAreaView`
+    background-color: #000;
+    flex: 1;
+`
+
+export const Banner = styled.View`
+    flex: 2;
+    padding: 15px;
+    background-color: #22bb83;
+`
+
+export const Section = styled.View`
+    flex: 4;
+    padding: 15px;
+`
+
+export const BannerLabel = styled.Text`
+    color: #fff;
+    font-size: 12px;
+    font-weight: bold;
+`
+
+export const BannerCDI  = styled.Text`
+    color: #fff;
+    text-align: center;
+    font-size: 14px;
+    margin-top: 20px;
+`
+export const BannerBalance = styled.Text`
+    color: #fff;
+    font-size: 30px;
+    font-weight: bold;
+    text-align: center;
+    margin: 15px 0;
+`
+export const BannerCashBack = styled.Text`
+    color: #fff;
+    text-align: center;
+    font-size: 14px;
+`
+
+export const FormsPayment = styled.Text`
+    color: #fff;
+    font-size: 16px;
+    font-weight: bold;
+    margin: 15px 0;
+
+`
